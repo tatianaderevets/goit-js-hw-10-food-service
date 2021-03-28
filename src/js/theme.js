21162c2a19e43932ac658275c99b0d4eca4e5d7b
tatianaderevets <72392848+tatianaderevets@users.noreply.github.com>
2021-03-28 13:57:34 +0300

    Update theme.js

--- src/js/theme.js
+++ src/js/theme.js
@@ -3,6 +3,35 @@ const Theme = {
   DARK: 'dark-theme',
 };
 
-const bodyRef = document.querySelector('body');
-const checboxRef = document.querySelector('theme-switch-toggle');
+    const bodyRef = document.querySelector('body');
+    const switcherRef = document.querySelector('#theme-switch-toggle')
 
+
+switcherRef.checked = localStorage.getItem('theme') === Theme.DARK
+
+console.log(Theme);
+
+const saveSettings = localStorage.getItem('theme') === null ? Theme.LIGHT : localStorage.getItem('theme')
+bodyRef.classList.add(saveSettings)
+
+const changeClass = (add, rem) => {
+bodyRef.classList.remove(rem)
+        bodyRef.classList.add(add)
+        localStorage.setItem('theme',add)
+}
+    
+
+switcherRef.addEventListener('change', ({target}) => {
+    console.dir(target);
+    if (target.checked) {
+        changeClass(Theme.DARK,Theme.LIGHT)
+        
+
+    }
+    else {
+        changeClass(Theme.LIGHT,Theme.DARK)
+    }
+    
+
+    
+})
